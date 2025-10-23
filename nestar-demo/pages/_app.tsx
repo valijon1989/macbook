28979d60f811217c85df19e580b2af50088ff5bf
valijon1989 <valijon.jon89@gmail.com>
2025-10-23 13:30:21 +0900

    B: feat: publish homepage screen component

---
+++ nestar-demo/pages/_app.tsx
@@ -0,0 +1,25 @@
+import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { light } from "@/scss/MaterialTheme";
+import { useState } from "react";
+import { ApolloProvider } from "@apollo/client";
+import client from "@/apollo/client";
+import "../scss/app.scss";
+import "../scss/pc/main.scss";
+import "../scss/mobile/main.scss";
+
+export default function App({ Component, pageProps }: AppProps) {
+  // @ts-ignore
+  const [ theme, setTheme ] = useState(createTheme(light));
+
+  // SocketIO, Redux, MUI, Apollo Client ...
+  return (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
+  );
+}
